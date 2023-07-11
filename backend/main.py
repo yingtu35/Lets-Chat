a1b1c899674e2f412b4cd3bb5b994133500c9495
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-07-11 14:39:17 +0800

    Serve frontend app from backend
    1. Move frontend folder(client) to backend
    2. Add health_check and version endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, session, render_template
+from flask import Flask, Response, session, send_from_directory
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
 from api.models import db, User, Room, Message
 from api.views import api_blueprints
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./client/build")
 app.register_blueprint(api_blueprints, url_prefix='/api')
 # CORS(app, origins=["http://localhost:3000", "ws://localhost:3000"], supports_credentials=True)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
@@ -21,6 +21,28 @@ db.init_app(app)
 # Create tables
 with app.app_context():
     db.create_all()
+
+# Serving frontend static files
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.get("/health")
+def health_check():
+    return Response("ok", 200)
+
+
+@app.get("/version")
+def get_version():
+    return Response("0.1.0", 200)
+
 
 @socketio.on("connect")
 def connect():
@@ -37,14 +59,16 @@ def connect():
         return
     # if user.rid != rid:
     #     return
-    
+
     join_room(rid)
-    emit("join", {"username": username, "message": "has enter the room"}, to=rid)
+    emit("join", {"username": username,
+         "message": "has enter the room"}, to=rid)
     # send({"username": username, "message": "has enter the room"}, to=room)
     print(f"{username} join the room")
     user.rid = rid
     room.num_users += 1
     db.session.commit()
+
 
 @socketio.on("disconnect")
 def disconnect():
@@ -67,12 +91,13 @@ def disconnect():
     #     return
     # if user.rid != rid:
     #     return
-    
+
     leave_room(rid)
-    emit("leave", {"username": username, "message": "has left the room"}, to=rid)
+    emit("leave", {"username": username,
+         "message": "has left the room"}, to=rid)
     # session.pop("room", None)
     print(f"{username} leave the room")
-    
+
     # TODO: Should write an automated script to delete all rooms with 0 users.
     if user.uid == room.host_uid:
         rid = session.get("room")
@@ -87,7 +112,7 @@ def disconnect():
                     break
             print(f"No user in the room. delete the room")
         # normal procedure, no refresh or closing page
-        elif rid: 
+        elif rid:
             print(f"room {rid} will be deleted")
             db.session.delete(room)
             db.session.commit()
@@ -112,6 +137,7 @@ def disconnect():
     #     db.session.delete(room)
     db.session.commit()
 
+
 @socketio.on("chat")
 def handle_chat(data):
     username = data.get("username")
@@ -128,7 +154,7 @@ def handle_chat(data):
         return
     if user.rid != rid:
         return
-    
+
     emit("message", {"username": username, "message": message}, to=rid)
     newMessage = Message(
         msg=message,
@@ -139,7 +165,8 @@ def handle_chat(data):
     db.session.add(newMessage)
     db.session.commit()
     print(f"{username}'s new message successfully added")
-        
+
+
 # Running app
 if __name__ == '__main__':
     socketio.run(app, debug=True)
